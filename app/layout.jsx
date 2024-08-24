5bc4d49a2e2d89177b178cea62eab27840126d14
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-24 13:49:47 +0000

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Vinas Beauty and Clothing",
+  description: "...where authentic products are being sold",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
