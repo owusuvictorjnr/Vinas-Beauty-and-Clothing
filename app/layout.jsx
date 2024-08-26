dc32bbea666eeb1b8d0d74c1a87aaaef8fb42a79
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-26 10:30:26 +0000

    refactor: maintain server side

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
