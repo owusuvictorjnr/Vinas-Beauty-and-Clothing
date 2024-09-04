17cf2edf1dcfd3c8f70865134cb8f2e8421c77b6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-09-04 11:40:34 +0000

    import hero page

--- app/(frontend)/page.jsx
+++ app/(frontend)/page.jsx
@@ -1,5 +1,15 @@
-
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col"></main>;
+  return (
+    <main className="flex min-h-screen flex-col">
+      <section>
+        <section>
+          {/* Hero Banner */}
+
+          <Hero />
+        </section>
+      </section>
+    </main>
+  );
 }
