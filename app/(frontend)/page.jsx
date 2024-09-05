fa4855a495b978c3784d006194799db51d234af6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-09-05 16:56:14 +0000

    import slide text component

--- app/(frontend)/page.jsx
+++ app/(frontend)/page.jsx
@@ -4,6 +4,7 @@ import FeaturedCollection from "@/components/FeaturedCollection/FeaturedCollecti
 import Hero from "@/components/Hero/Hero";
 import NewArrival from "@/components/NewArrival/NewArrival";
 import OfficialWare from "@/components/OfficalWare/OfficialWare";
+import SlideText from "@/components/SlidingText/SlidingText";
 
 export default function Home() {
   const promoEndDate = "2024-12-31T23:59:59"; // Set your promo end date
@@ -31,12 +32,19 @@ export default function Home() {
           <PromoCountdown targetDate={promoEndDate} />
         </section>
 
+        {/* Featured product */}
         <section>
           <FeaturedCollection />
         </section>
 
+        {/* New arrival */}
         <section>
           <NewArrival />
+        </section>
+
+        {/* Text Slider */}
+        <section>
+          <SlideText />
         </section>
       </section>
     </main>
